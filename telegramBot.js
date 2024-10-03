515ca2fffd4e578b23491f8c837dd1fbc2c49e6b
christiancampos123 <christiancampos1996@gmail.com>
2024-10-03 09:34:22 +0200

    initial commit

---
+++ telegramBot.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+async function wallapopBotNotification(message) {
+  const TelegramService = require('./services/telegram-service'); // Ajusta la ruta según sea necesario
+  const telegramService = new TelegramService();
+
+  messages = message.split("---\n")
+
+  messages.forEach(e => {
+    telegramService.sendMessage(process.env.chatId, e);
+  });
+}
+wallapopBotNotification("hey")
